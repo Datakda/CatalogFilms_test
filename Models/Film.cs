c98e711cfca8faf59b86a54d088f9b9801f5212c
Datakda <gagan9393@gmail.com>
2021-05-01 20:14:47 +0300

    Добавил модель фильма

---
+++ Models/Film.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_films_test.Models
+{
+    public class Film
+    {   [Key]
+        public int Id;
+
+        public string Name;
+
+        public string Description;
+
+        public int Yers;
+
+        public string Producer;
+
+        public int OwnerUserID;
+
+
+    }
+}
