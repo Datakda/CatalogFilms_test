a36000c8e794c1a5004205c073b0aa981ee0ff37
Datakda <gagan9393@gmail.com>
2021-05-01 23:14:13 +0300

    Добавил новый контролер  FilmController

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_films_test.Controllers
+{
+    public class FilmController : Controller
+    {
+
+        //списка всех фильмов (с пагинацией);
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //страница создания нового фильма.
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+       // страница редактирования данных о фильме;
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+
+        //страница одного отдельного фильма;
+
+    }
+}
